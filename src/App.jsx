4ea2b6726584cfcbac9dc68eb6e8c735218c7ca2
Pankhuri33 <154539221+Pankhuri33@users.noreply.github.com>
2025-07-11 12:43:58 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   return (
     <>
+      <Toaster />
       <BrowserRouter>
         <Routes>
-            <Route index element={<Home />} />
-            <Route path="*" element={<NotFound />} />
-
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
