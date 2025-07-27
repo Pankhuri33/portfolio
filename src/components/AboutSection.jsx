aa863f4bebfc4e8f3623fcfb09a905c0375a589a
Pankhuri33 <154539221+Pankhuri33@users.noreply.github.com>
2025-07-27 15:14:24 +0000

    commit

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -36,8 +36,9 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
 
+
               <a
-                href="https://drive.google.com/file/d/1Q98jxktpIm3NLYiJgOBOnRHvVxQNgZln/view?usp=sharing"
+                href="https://drive.google.com/file/d/1PfjptY78tSJgQ1uzGV1SFFt-h6Y28aMg/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
